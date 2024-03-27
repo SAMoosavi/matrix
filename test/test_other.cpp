fe73274275643108997375628090f4a07aa8944c
SAMoosavi <moosavi238@gmail.com>
2024-03-28 00:11:11 +0330

    add test and move googletest to this

---
+++ test/test_other.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Matrix.h"
+
+// Define your test cases here
+TEST(TestCaseName, TestName) {
+	// Your test code goes here
+	ASSERT_EQ(1, 1);
+	Matrix<int> a(5,5);
+
+	ASSERT_EQ(a.at(2,3),0);
+
+}
+
+// Add more test cases as needed
