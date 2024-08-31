a96e1ce5314d9c3f8946d3976ebeb403b18eab54
Arrash03 <atv1382@gmail.com>
2024-08-31 22:08:27 +0330

    .

--- test/polynomialFunctionality.cpp
+++ test/polynomialFunctionality.cpp
@@ -46,6 +46,28 @@ TEST_F(SharePolynomial, PolynomialSumWithElementTest)
 	EXPECT_EQ(Polynomial(expected_result), result);
 }
 
+TEST_F(SharePolynomial, PolynomialSumEqualWithPolynomialTest)
+{
+	Coefficient expected_result = coefficients2;
+	for (size_t i = 0; i < coefficients1.size(); i++)
+		expected_result[i] += coefficients1[i];
+
+	auto polynomial = Polynomial(coefficients1);
+	polynomial += Polynomial(coefficients2);
+
+	EXPECT_EQ(Polynomial(expected_result), polynomial);
+}
+
+TEST_F(SharePolynomial, PolynomialSumEqualWithElementTest)
+{
+	Coefficient expected_result = coefficients1;
+	expected_result[0] += element;
+
+	auto polynomial = Polynomial(coefficients1);
+	polynomial += element;
+	EXPECT_EQ(Polynomial(expected_result), polynomial);
+}
+
 class PolynomialTest: public testing::TestWithParam<SolveParameter> {
 };
 
