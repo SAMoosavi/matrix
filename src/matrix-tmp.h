237f678ef40781ad1c4ca9e673678a32b2e53a2b
SAMoosavi <moosavi238@gmail.com>
2024-08-31 22:31:53 +0330

    add test for construct wit value

--- src/matrix-tmp.h
+++ src/matrix-tmp.h
@@ -7,21 +7,20 @@
 
 template <Elementable Element>
 Matrix<Element>::Matrix()
-	: row(0), col(0), table({{}}){};
+	: row(0), col(0), table({{}}){}
 
 template <Elementable Element>
 Matrix<Element>::Matrix(size_t row, size_t col)
-	: row(row), col(col), table(row, RowType(col, 0)){};
+	: row(row), col(col), table(row, RowType(col, 0)){}
 
 template <Elementable Element>
 template <template <Containerable> typename Container>
 Matrix<Element>::Matrix(const Container<Container<Element>>& matrix)
-	: row(matrix.size()), col(matrix[0].size()), table(row, RowType(col, 0))
+	: row(matrix.size()), col(matrix.begin()->size()), table(0)
 {
-	for (int row_index = 0; row_index < row; ++row_index)
-		for (int col_index = 0; col_index < col; ++col_index)
-			table[row_index][col_index] = matrix[row_index][col_index];
-};
+	for (const auto& row_of_matrix: matrix)
+		table.emplace_back(row_of_matrix);
+}
 
 template <Elementable Element>
 template <typename OtherElement>
--- test/matrixFunctionality.cpp
+++ test/matrixFunctionality.cpp
@@ -5,20 +5,44 @@
 using namespace ::testing;
 
 class MatrixFunctionality: public Test {
-public:
-	const size_t DEFAULT_VALUE_OF_MATRIX_CONSTRUCTOR = 0;
 };
 
-TEST_F(MatrixFunctionality, default_values_of_table_in_matrix_shuld_be_zero)
+TEST_F(MatrixFunctionality, creat_matrix_with_size_should_creat_matrix_with_size_and_set_value_of_each_cell_zero)
 {
 	constexpr size_t NUMBER_OF_RAW = 4;
-	constexpr size_t NUMBER_OF_COL = 4;
+	constexpr size_t NUMBER_OF_COL = 5;
+	const int DEFAULT_VALUE_OF_MATRIX_CONSTRUCTOR = 0;
 
 	Matrix<int> matrix(NUMBER_OF_RAW, NUMBER_OF_COL);
-	const auto& table = matrix.get_table();
-	EXPECT_EQ(matrix.get_number_of_row(), NUMBER_OF_RAW);
-	for (const auto& row: table) {
-		EXPECT_THAT(row, SizeIs(NUMBER_OF_COL));
-		EXPECT_THAT(row, Each(DEFAULT_VALUE_OF_MATRIX_CONSTRUCTOR));
-	}
+	std::vector<std::vector<int>> TABLE_OF_MATRIX(NUMBER_OF_RAW, std::vector<int>(NUMBER_OF_COL,
+		DEFAULT_VALUE_OF_MATRIX_CONSTRUCTOR));
+	EXPECT_EQ(matrix.get_table(), TABLE_OF_MATRIX);
+}
+
+TEST_F(MatrixFunctionality, creat_matrix_with_vector)
+{
+	const std::vector<std::vector<double>> INITIALIZE_DATA_OF_MATRIX = {
+			{1.2, 2.3, 3, 4},
+			{1, 2, 3.5, 4},
+			{5, 6, 7, 8.9}
+	};
+
+	Matrix<double> matrix(INITIALIZE_DATA_OF_MATRIX);
+	EXPECT_EQ(matrix.get_table(), INITIALIZE_DATA_OF_MATRIX);
+}
+
+TEST_F(MatrixFunctionality, creat_matrix_with_intializer_list)
+{
+	const std::initializer_list<std::initializer_list<uint32_t>> INITIALIZE_LIST_OF_MATRIX = {
+			{1, 2, 3, 4},
+			{1, 2, 3, 4},
+			{5, 6, 7, 8}
+	};
+
+	std::vector<std::vector<uint32_t>> table_of_matrix;
+	for (const auto& row_of_matrix: INITIALIZE_LIST_OF_MATRIX)
+		table_of_matrix.emplace_back(row_of_matrix);
+
+	Matrix<uint32_t> matrix(INITIALIZE_LIST_OF_MATRIX);
+	EXPECT_THAT(matrix.get_table(), Eq(table_of_matrix));
 }
