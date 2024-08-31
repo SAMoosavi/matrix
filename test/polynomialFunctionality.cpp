2c5d6849fd29895a7264d0ebf476a26c25d1d6ae
Arrash03 <atv1382@gmail.com>
2024-08-31 21:13:29 +0330

    Add solve unit tests

--- test/polynomialFunctionality.cpp
+++ test/polynomialFunctionality.cpp
@@ -1,58 +1,34 @@
-#include <vector>
+#include <algorithm>
+#include <functional>
 #include <tuple>
+#include <vector>
 
 #include "polynomial.h"
 #include <gtest/gtest.h>
 
 using namespace std;
 
-template <typename T>
-bool is_vectors_equal(const vector<T>& vec1, const vector<T>& vec2)
-{
-	if (vec1.size() != vec2.size())
-		return false;
-	for (const auto& iterator1: vec1) {
-		bool is_found = false;
-		for (const auto& iterator2: vec2) {
-			if (Polynomial<T>::compare_with_precision(iterator1, iterator2, 6))
-				is_found = true;
-		}
-		if (!is_found)
-			return false;
-	}
-	return true;
-}
-
 using Coefficient = vector<int>;
 using Root = vector<double>;
-	using SolveParameter = tuple<bool, Coefficient, Root>;
+using SolveParameter = tuple<Coefficient, Root>;
 
-	class PolynomialTest: public testing::TestWithParam<SolveParameter> {
+class PolynomialTest: public testing::TestWithParam<SolveParameter> {
 };
 
 TEST_P(PolynomialTest, PolynomialSecondDegree)
 {
-    const vector<int> coefficients = {2, -3, 1};
-	Polynomial<int> polynomial(coefficients);
+	const Coefficient coefficients = std::get<0>(GetParam());
+	const Root expected_roots = std::get<1>(GetParam());
 
-    if (std::get<0>(GetParam()))
-    {
-		const Root roots = polynomial.solve(100);
-		const Root expected_roots = {1, 2};
-		EXPECT_TRUE(is_vectors_equal(roots, expected_roots));
-    } else
-    {
-		const Coefficient new_coefficients = std::get<1>(GetParam());
-		const Root expected_roots = std::get<2>(GetParam());
+	const auto polynomial = Polynomial(coefficients);
+	Root roots = polynomial.solve(100);
+	std::sort(roots.begin(), roots.end());
 
-		polynomial *= Polynomial(new_coefficients);
-		const Root roots = polynomial.solve(100);
-		EXPECT_TRUE(is_vectors_equal(roots, expected_roots));
-    }
+	EXPECT_EQ(roots,expected_roots);
 }
 
 INSTANTIATE_TEST_SUITE_P(SolveParameters, PolynomialTest, testing::Values(
-    make_tuple(true, vector<int>{2, -3, 1}, vector<double>{1, 2}),
-    make_tuple(false, vector<int>{0, 1}, vector<double>{0, 1, 2}),
-    make_tuple(false, vector<int>{0, -3, 1}, vector<double>{0, 1, 2, 3})
+	make_tuple(vector<int>{2, -3, 1}, vector<double>{1, 2}),
+	make_tuple(vector<int>{0, 2, -3, 1}, vector<double>{0, 1, 2}),
+	make_tuple(vector<int>{0, -6, 11, -6, 1}, vector<double>{0, 1, 2, 3})
 ));
